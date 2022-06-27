b88a511fa9b94740cee0afd9b4e1cfbaa4a28a50
SGE176 <s.ge.176@westcliff.edu>
2022-06-27 18:44:36 -0500

    frontend finish

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Sijia`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer"]'><span className="wrap">{text}</span></span></h1>
-                  <p>it to make a type specimen book.</p>
+                  <p>Personal Introduction Page</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,9 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -26,19 +29,19 @@ export const Projects = () => {
       imgUrl: projImg3,
     },
     {
-      title: "Fitness Center",
-      description: "Workout & Healthy Lifestyle",
-      imgUrl: projImg2,
+      title: "Weather App",
+      description: "Life & Convenience",
+      imgUrl: projImg4,
     },
     {
-      title: "Fitness Center",
-      description: "Workout & Healthy Lifestyle",
-      imgUrl: projImg2,
+      title: "Shop Cart",
+      description: "Shopping & Application",
+      imgUrl: projImg5,
     },
     {
-      title: "Fitness Center",
-      description: "Workout & Healthy Lifestyle",
-      imgUrl: projImg2,
+      title: "Chicago Shelter",
+      description: "Pet & Adoption",
+      imgUrl: projImg6,
     }
   ];
 
@@ -51,17 +54,17 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>After learning Full Stack Web deveopment in WestCliff University, I'm glad to share stories of what I've learned in this great bootcamp. These projects help me understand various languages like HTML, CSS, Javascript.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      <Nav.Link eventKey="first">Past Projects</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      <Nav.Link eventKey="second">Get Connected</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link eventKey="third">Build your Website</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
@@ -83,7 +86,7 @@ export const Projects = () => {
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <p>If you are inspired or intersted in my projects, don't forget to leave your contact information to get us connected! As a full-Stack Web Developer, I have the programming expertise and industry-specific experience to build, integrate, and customize your website or application to align perfectly with your vision. </p>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
